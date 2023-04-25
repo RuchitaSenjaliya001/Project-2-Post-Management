ec454c138788cab58e4bc18a96155ccaa58dc760
Ruchita <ruchita.s@soltelabs.com>
2023-04-25 15:41:24 +0530

    fetch data

--- src/components/UI/Pagination.jsx
+++ src/components/UI/Pagination.jsx
@@ -1,29 +1,49 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from "react";
+import Button from "./Button";
 
 export default function Pagination() {
-    const [page, setPage] = useState(1)
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+            const data = await response.json()
+        }
+    }, [])
+    const [page, setPage] = useState(1);
     const totalPage = 10;
 
     const prevPage = () => {
-        setPage(page - 1)
+        setPage(page - 1);
         if (page <= 1) {
-            setPage(1)
+            setPage(1);
         }
-    }
+    };
     const nextPage = () => {
-        setPage(page + 1)
+        setPage(page + 1);
         if (page >= totalPage) {
-            setPage(totalPage)
+            setPage(totalPage);
         }
-    }
+    };
 
     return (
         <>
-            <div className="flex gap-6 justify-center my-5 ">
-                <button onClick={prevPage}>PREV</button>
-                <p>{page} of {totalPage}</p>
-                <button onClick={nextPage} >NEXT</button>
+            <div className="flex items-center gap-6 justify-center my-5 ">
+                <Button
+                    onClick={prevPage}
+                    type="button"
+                    title="PREV"
+                    className="bg-[#201d75] hover:bg-[#121056]"
+                ></Button>
+                <p>
+                    {page} of {totalPage}
+                </p>
+                <Button
+                    onClick={nextPage}
+                    type="button"
+                    title="NEXT"
+                    className="bg-[#201d75] hover:bg-[#121056]"
+                ></Button>
             </div>
         </>
-    )
+    );
 }
